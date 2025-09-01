f20b881f5758b46ff97b401c70fa5b5ab18da652
IVYTEGIOJAN302005 <iivy74402@gmail.com>
2025-09-01 22:43:30 +0800

    saved

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-console.log("First message");
+// console.log("First message");
 
-setTimeout(() => {
-  console.log("Second message");
-}, 2000);
+// setTimeout(() => {
+//   console.log("Second message");
+// }, 2000);
 
-console.log("Third message");
+// console.log("Third message");
 
-//Part 2
-fetch("https://api.adviceslip.com/advice")
-  .then(response => response.json())
-  .then(data => {
-    console.log("Here's your advice:", data.slip.advice);
-  })
-  .catch(error => console.error("Error:", error));
+// //Part 2
+// fetch("https://api.adviceslip.com/advice")
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log("Here's your advice:", data.slip.advice);
+//   })
+//   .catch(errora => console.error("Error:", error));
 
 
- //Part 3
- async function getAdvice() {
-  try {
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const data = await response.json();
-    console.log("Here's your advice:", data.slip.advice);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
+//  //Part 3
+//  async function getAdvice() {
+//   try {
+//     const response = await fetch("https://api.adviceslip.com/advice");
+//     const data = await response.json();
+//     console.log("Here's your advice:", data.slip.advice);
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// }
 
-getAdvice();
+// getAdvice();
 
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+// fetch("https://type.fit/api/quotes")
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//   });
